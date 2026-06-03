2f512eaec3a31b1be40e591a4d5909583dcf48f8
Papa Ours <134229865+PapaOursPolaire@users.noreply.github.com>
2026-06-03 21:35:56 +0200

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -1,7 +1,9 @@
-console.log({
++console.log({
   currentTrackId: window.SpotifyLyricsSaver?.state?.currentTrackId,
   savedTrackIds: [...(window.SpotifyLyricsSaver?.state?.savedTrackIds ?? [])],
   savedScore: window.SpotifyLyricsSaver?.state?.savedScore,
   pending: Object.keys(window.SpotifyLyricsSaver?.state?.pending ?? {}),
   totalSaved: window.SpotifyLyricsSaver?.state?.totalSaved,
 })
+VM256:1 {currentTrackId: '6C0SXfD7rR46MpWK5VJSEw', savedTrackIds: Array(2), savedScore: {…}, pending: Array(0), totalSaved: 2}currentTrackId: "6C0SXfD7rR46MpWK5VJSEw"pending: []savedScore: {4GB4R2CJe5kLrjWTusDR8p: 60, 6C0SXfD7rR46MpWK5VJSEw: 60}savedTrackIds: (2) ['4GB4R2CJe5kLrjWTusDR8p', '6C0SXfD7rR46MpWK5VJSEw']totalSaved: 2[[Prototype]]: Object
+NaN
