c19a0b1bdbfc64f75f3a9e634dfd04d394922dcb
Papa Ours <134229865+PapaOursPolaire@users.noreply.github.com>
2026-06-06 19:10:13 +0200

    Update drm.js

--- drm.js
+++ drm.js
@@ -1,1 +1,10 @@
-cat /tmp/beartify-$(ls -t /tmp | grep beartify- | grep -v honeypot | head -1)/playlist.m3u8
+papaours@papaours:~$ cat /tmp/beartify-$(ls -t /tmp | grep beartify- | grep -v honeypot | head -1)/playlist.m3u8
+cat: /tmp/beartify-beartify-1780767530197621896f/playlist.m3u8: Aucun fichier ou dossier de ce nom
+papaours@papaours:~$ 
+
+
+
+
+
+
+
