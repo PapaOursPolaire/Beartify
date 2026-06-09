53b16191dda26aeb266709843b58d44ee1eadd8b
Papa Ours <134229865+PapaOursPolaire@users.noreply.github.com>
2026-06-09 11:47:59 +0200

    Update drm.sh

--- drm.sh
+++ drm.sh
@@ -1,1 +1,16 @@
-sudo journalctl -u beartify-drm -n 20 --no-pager | grep -i "extension\|error\|require\|Cannot"
+papaours@papaours:~$ sudo journalctl -u beartify-drm -n 20 --no-pager | grep -i "extension\|error\|require\|Cannot"
+[sudo] Mot de passe de papaours : 
+papaours@papaours:~$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
