b69f50f907d0c60b1235664f8cb61e86d51daa51
Papa Ours <134229865+PapaOursPolaire@users.noreply.github.com>
2026-06-09 11:48:33 +0200

    Update drm.sh

--- drm.sh
+++ drm.sh
@@ -1,16 +1,2 @@
-papaours@papaours:~$ sudo journalctl -u beartify-drm -n 20 --no-pager | grep -i "extension\|error\|require\|Cannot"
-[sudo] Mot de passe de papaours : 
-papaours@papaours:~$ 
-
-
-
-
-
-
-
-
-
-
-
-
-
+ls -la /opt/beartify-drm/extensions.js
+node -e "const app={get:()=>{}}; require('/opt/beartify-drm/extensions.js')(app); console.log('OK')"
