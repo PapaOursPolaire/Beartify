1e5a8848c4625c3f2ae968fe2bf1561ab181ac22
Papa Ours <134229865+PapaOursPolaire@users.noreply.github.com>
2026-06-28 01:28:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+Une seule ligne changée. Dans le bloc Discord Tauri, j'ai remplacé :
+
+```js
+window.__TAURI__.core.invoke('plugin:shell|open', { path: discordUrl, openWith: null })
+```
+
+par :
+
+```js
+const shellOpenArgs = _FB_IS_ANDROID
+  ? { path: discordUrl }
+  : { path: discordUrl, openWith: null };
+window.__TAURI__.core.invoke('plugin:shell|open', shellOpenArgs)
+```
+
+C'est tout. `openWith: null` fait planter `plugin:shell|open` sur Android car ce paramètre n'est pas reconnu sur mobile. Sur Desktop il reste inchangé.
